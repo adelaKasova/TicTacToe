33304cff7426f14d4da00dfc39237ab4ac11cfbb
AKratka <Adelakratka@teta.eu>
2018-08-13 08:58:11 +0200

    úpravy

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import Board from './game/Board';
 import Menu from './Menu';
-import {connect} from 'react-redux';
 
 class Main extends Component {
 	render(){
@@ -19,14 +18,4 @@ class Main extends Component {
 	};
 };
 
-function mapStateToProps(state, props) {
-	return {
-		winner: state.reducer.winner,
-		squares: state.reducer.squares,
-		stepNumber: state.reducer.stepNumber,
-		xIsNext: state.reducer.xIsNext,
-		isEnd: state.reducer.isEnd
-	};
-};
-
-export default connect(mapStateToProps) (Main);
+export default Main;
